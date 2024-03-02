0beaa0d62e7b072a31e70d934c1d65033d4be7aa
Nishant Singh <142491474+nishantsingh6@users.noreply.github.com>
2024-03-02 22:56:13 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='w-full border shadow-md py-4 fixed top-0 bg-white '>
       <header className='text-center'>
-        <h1 className='text-3xl font-bold'>Blogs Website</h1>
+        <h1 className='text-3xl font-bold'>Blog Website</h1>
       </header>
     </div>
   )
